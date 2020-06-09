e97305534a43cee51edc467a18a0de3383ccbee0
Monadic Cat <greenmonadcat@gmail.com>
2020-06-09 18:56:29 -0500

    improve dice command

--- commands/dice.js
+++ commands/dice.js
@@ -1,4 +1,5 @@
 const Command = require("../command.js");
+const mice = require('@monadic_cat/mice-js');
 module.exports = rigidbot => {
 	const utils = rigidbot.utils;
 	rigidbot.commands.push(new Command({
@@ -11,79 +12,19 @@ module.exports = rigidbot => {
 		],
 		alias: ["roll", "diceroll", "rolldice"],
 		run: async e => {
-			if (e.args.length == 0) {
+			try {
+				let result = mice.roll(e.args.join(" "));
+				new utils.Message({
+					channel: e.channel,
+					user: e.user,
+				}, result.display()).create();
+				return true;
+			} catch (e) {
+				new utils.Message({
+					channel: e.channel,
+					user: e.user,
+				}, e).create();
 				return false;
-			}
-			var res = 0;
-			var action = "+";
-			var success = true;
-			var mode = true;
-			var iters = 0;
-			var maxiters = 256;
-			for (var i = 0; i < e.args.length; i++) {
-				const arg = e.args[i].toLowerCase();
-				if (!mode) {
-					action = arg;
-					mode = true;
-					continue;
-				}
-				var cur = 0;
-				const pos = +arg;
-				if (pos == pos && pos >= 0) {
-					cur = pos;
-				} else {
-					const sides = arg.split(/[dD]/);
-					if (sides.length != 2 && sides.length != 1) {
-						success = false;
-						break;
-					}
-					var amt = sides[0];
-					if (amt.trim() == "") {
-						amt = "1";
-					}
-					const ind = +amt;
-					const dice = sides[1];
-					const size = +dice;
-					if (ind == ind && ind >= 0 && size == size && size > 0) {
-						for (var i = 0; i < ind; i++) {
-							iters++;
-							if (iters > maxiters) {
-								success = false;
-								break;
-							}
-							cur += Math.floor(Math.random() * size) + 1;
-						}
-						if (!success) break;
-					} else {
-						success = false;
-						break;
-					}
-				}
-				if (action == "+") {
-					res += cur;
-				} else if (action == "-") {
-					res -= cur;
-				}
-				mode = false;
-			}
-			if (!success && iters <= maxiters) {
-				new utils.Message({
-					channel: e.channel,
-					user: e.user
-				}, "That is an invalid dice expression.").create();
-				return true;
-			} else if (!success) {
-				new utils.Message({
-					channel: e.channel,
-					user: e.user
-				}, "You may not perform that many calculations.").create();
-				return true;
-			} else {
-				new utils.Message({
-					channel: e.channel,
-					user: e.user
-				}, "The result of that dice roll was **" + res + "**.").create();
-				return true;
 			}
 		}
 	}));
