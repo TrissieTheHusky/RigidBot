d789d9537cbb1ac123c08c4f6c587d02241ede8a
pxpcandy <39356292+pxpcandy@users.noreply.github.com>
2020-06-10 16:52:37 -0500

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -21,14 +21,14 @@ module.exports = rigidbot => {
 			}
 			const originalStatus = member.presence.status;
 			var status;
-			if (originalStatus == "online")
+			if(originalStatus == "online")
 				status = "Online";
-			else if (originalStatus == "offline")
+			if(originalStatus == "offline")
 				status = "Offline";
-			else if (originalStatus == "dnd")
-				status = "Do Not Disturb";
-			else if (originalStatus == "idle")
+			if(originalStatus == "idle")
 				status = "Idle";
+			if(originalStatus == "do not disturb")
+				status = "DND";
 			var rolelist = [];
 			member.roles.cache.array().forEach(role => {
 				if (role.id == e.guild.roles.everyone.id) {
